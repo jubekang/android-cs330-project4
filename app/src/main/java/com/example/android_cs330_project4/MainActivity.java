f167377cb485cfd158ebdcbba35bc4804eb0bd16
JunbeomKang <legokjuny@naver.com>
2023-06-07 17:15:01 +0900

    Cleaning Code

--- app/src/main/java/com/example/android_cs330_project4/MainActivity.java
+++ app/src/main/java/com/example/android_cs330_project4/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.android_cs330_project4;
 
-import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
@@ -10,18 +10,17 @@ import android.hardware.SensorManager;
 import android.media.AudioRecord;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import org.checkerframework.checker.units.qual.A;
 import org.tensorflow.lite.support.audio.TensorAudio;
 import org.tensorflow.lite.support.label.Category;
 import org.tensorflow.lite.task.audio.classifier.AudioClassifier;
@@ -32,11 +31,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    public String TAG = "MyApp_dont_fall";
+
     /**
      * Variable for Permissions
      */
     public String[] permissions = new String[]{"Manifest.permission.RECORD_AUDIO"};
-    private int PERMISSIONS_REQUEST = 0x0000001;
 
     /**
      * Variable for silence classification
@@ -44,11 +44,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private static final String YAMNET_MODEL = "lite-model_yamnet_classification_tflite_1.tflite";
     private static final float SOUND_THRESHOLD = (float) 0.5;
     private AudioRecord recorder = null;
-    private long REFRESH_INTERVAL_MS = 1000L;
-    private long NUMBER_OF_ITERATION = 20;
-    private long ENTIRE_TIME = REFRESH_INTERVAL_MS * NUMBER_OF_ITERATION;
+    private final long REFRESH_INTERVAL_MS = 1000L;
+    private final long NUMBER_OF_ITERATION = 10;
+    private final long ENTIRE_TIME = REFRESH_INTERVAL_MS * NUMBER_OF_ITERATION;
+
     public int Silence_Count = 0;
     public int Classifications_Count = 0;
+    public int Screaming_Count = 0;
 
     /**
      * Variable for current state
@@ -69,8 +71,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
      * Variable for IMU Sensor
      */
     private SensorManager sensorManager;
-    private Sensor sensor;
-    private float sensor_threshold = (float) 40;
 
     /**
      * Variable for Timer
@@ -82,24 +82,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /**
-         * link button and variable
-         */
-        _display_onoff = (TextView) findViewById(R.id.display_onoff);
-        _status_log = (TextView) findViewById(R.id.status);
-        _onoff = (Button) findViewById(R.id.button_monitoring_onoff);
-        _help = (Button) findViewById(R.id.button_help);
-        _Background = (ConstraintLayout) findViewById(R.id.Backgroud);
+        // link button and variable
+        _display_onoff = findViewById(R.id.display_onoff);
+        _status_log = findViewById(R.id.status);
+        _onoff = findViewById(R.id.button_monitoring_onoff);
+        _help = findViewById(R.id.button_help);
+        _Background = findViewById(R.id.Backgroud);
 
-        /**
-         * Initialization for sensor, timer, and permission requesting
-         */
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
-                == PackageManager.PERMISSION_DENIED)
-            requestPermissions (new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        // Initialization for sensor, timer, and permission requesting
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    == PackageManager.PERMISSION_DENIED) {
+                requestPermissions (permissions, 1);
+                break;
+            }
+        }
         setUpSensorStuff();
         CountDownTimerInit();
-
     }
 
     /**
@@ -107,12 +106,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
      */
     private void setUpSensorStuff() {
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         if (sensor != null) {
             sensorManager.registerListener(this,
                     sensor,
-                    sensorManager.SENSOR_DELAY_GAME,
-                    sensorManager.SENSOR_DELAY_GAME);
+                    SensorManager.SENSOR_DELAY_GAME,
+                    SensorManager.SENSOR_DELAY_GAME);
         }
     }
 
@@ -125,16 +124,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             // Executed at every timer interrupt
             public void onTick(long millisUntilFinished) {
                 startAudioClassification();
-               // _display_onoff.setText(String.format("Count : %d %d", Silence_Count, Classifications_Count));
-                String progress = "Detecting Sound .";
-                for (int i = 0; i < Classifications_Count % 4; i++) progress += " .";
-                _display_onoff.setText(progress);
+                StringBuilder progress = new StringBuilder("Detecting Sound .");
+                for (int i = 0; i < Classifications_Count % 4; i++) progress.append(" .");
+                _display_onoff.setText(progress.toString());
             }
             // Executed at finish of countdown timer
             public void onFinish() {
-                _display_onoff.setTextSize(50);
-                _display_onoff.setText("Detection Finished.");
+                // show Classification result using toast
+                @SuppressLint("DefaultLocale")
+                String result = String.format(
+                        "# Silence number = %d\n" +
+                        "# Screaming number = %d",
+                        Silence_Count, Screaming_Count);
+                Toast.makeText (getApplicationContext(), result, Toast.LENGTH_LONG).show();
+
+                Log.d(TAG," Detection Finished.");
                 stopAudioClassification();
+
+                if ((Silence_Count + Screaming_Count) > NUMBER_OF_ITERATION * 0.75 &&  _help.getText().toString().equals("I'm OK")) {help();}
+                else {solve();}
             }
         };
     }
@@ -146,20 +154,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void enable_audio_classifier() {
         _display_onoff.setTextSize(50);
         countDownTimer.start();
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                TextView textView = (TextView) _help;
-                Log.d("MytensorAudio_java", textView.getText().toString());
-                if (Silence_Count > 15 && textView.getText().toString().equals("I'm OK")) {
-                    help();
-                } else {
-                    solve();
-                }
-            }
-        }, ENTIRE_TIME);
-
     }
 
     /**
@@ -172,6 +166,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             TensorAudio tensor = classifier.createInputTensorAudio();
 
             if (recorder == null) {
+                Log.d(TAG," Detection Start.");
                 recorder = classifier.createAudioRecord();
                 recorder.startRecording();
             }
@@ -181,17 +176,38 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             List<Category> filterModelOutput = output.get(0).getCategories();
 
             Classifications_Count++;
+            float silence_score = -1;
+            float screaming_score = -1;
             for (Category c : filterModelOutput) {
                 if (c.getLabel().equals("Silence")) {
-                    Log.d("MytensorAudio_java", " label : " + c.getLabel() + " score : " + c.getScore());
-                    if (c.getScore() > SOUND_THRESHOLD) {
+                    Log.d(TAG, " label : " + c.getLabel() + " | score : " + c.getScore() + " (Iteration " + Classifications_Count + ")");
+                    silence_score = c.getScore();
+                    if (silence_score > SOUND_THRESHOLD && screaming_score != -1 && silence_score > screaming_score) {
+                        Silence_Count++;
+                        break;
+                    }
+                    else if (screaming_score > SOUND_THRESHOLD && screaming_score > silence_score) {
+                        Screaming_Count++;
+                        break;
+                    }
+                }
+                else if (c.getLabel().equals("Speech")) {
+                    Log.d(TAG, " label : " + c.getLabel() + " | score : " + c.getScore() + " (Iteration " + Classifications_Count + ")");
+                    screaming_score = c.getScore();
+                    if (screaming_score > SOUND_THRESHOLD && silence_score != -1 && screaming_score > silence_score) {
+                        Screaming_Count++;
+                        break;
+                    }
+                    else if (silence_score > SOUND_THRESHOLD && silence_score > screaming_score ) {
                         Silence_Count++;
                         break;
                     }
                 }
             }
 
-        } catch (IOException e) {}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -206,25 +222,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
      * Button Control for upper button
      */
     public void monitoring_onoff(View view) {
-        /**
-         * If monitoring is on, and try to off monitoring
-         */
+        // If monitoring is on, and try to off monitoring
         if (is_monitoring) {
             is_monitoring = false;
             is_fall_detected = false;
             Silence_Count = 0;
+            Screaming_Count = 0;
             Classifications_Count = 0;
-            _help.setEnabled(false);
             normal_ui_setup();
 
         }
-        /**
-         * monitoring is off, and try to enable monitoring
-         */
+        // monitoring is off, and try to enable monitoring
         else {
             monitoring_ui_setup();
             is_monitoring = true;
-            _help.setEnabled(true);
         }
     }
 
@@ -257,11 +268,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void solve() {
         is_fall_detected = false;
         monitoring_ui_setup();
+
+        Classifications_Count = 0;
+        Silence_Count = 0;
+        Screaming_Count = 0;
     }
 
     /**
      * Get the information of IMU gyroscope sensor
      */
+    @SuppressLint("DefaultLocale")
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE && is_monitoring && !is_fall_detected) {
@@ -277,7 +293,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     "angv_z: %.2f rad/s\n", rad_x, rad_y, rad_z));
             _status_log.setTextSize(40);
 
-            if (sensor_threshold < Math.pow(rad_x, 2) + Math.pow(rad_z, 2)) {
+            float SENSOR_THRESHOLD = 40;
+            if (SENSOR_THRESHOLD < Math.pow(rad_x, 2) + Math.pow(rad_z, 2)) {
                 is_fall_detected = true;
                 fall_detected_ui_setup();
                 enable_audio_classifier();
@@ -286,9 +303,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        return;
-    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
     @Override
     protected void onDestroy() {
@@ -297,7 +312,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     /**
-     * dispaly mode change to normal mode
+     * display mode change to normal mode
      */
     private void normal_ui_setup() {
         _onoff.setText(R.string.button_label_monitoring_start);
@@ -306,7 +321,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         _help.setBackgroundColor(getColor((R.color.blue)));
         _help.setTextColor(getColor((R.color.white)));
-        _help.setText("Help!");
+        _help.setText(R.string.button_label_help);
+        _help.setEnabled(false);
 
         _status_log.setText(R.string.status_initial_value);
         _status_log.setTextSize(100);
@@ -315,13 +331,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         _display_onoff.setText(R.string.display_off);
         _display_onoff.setTextSize(48);
         _display_onoff.setTextColor(getColor((R.color.blue)));
-        _display_onoff.setBackground(getDrawable(R.drawable.borderline_blue));
+        _display_onoff.setBackground(AppCompatResources.getDrawable(this, R.drawable.borderline_blue));
 
         _Background.setBackgroundColor(getColor((R.color.white)));
     }
 
     /**
-     * dispaly mode change to monitoring mode
+     * display mode change to monitoring mode
      */
     private void monitoring_ui_setup() {
         _onoff.setText(R.string.button_label_monitoring_stop);
@@ -330,35 +346,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         _help.setBackgroundColor(getColor((R.color.red)));
         _help.setTextColor(getColor((R.color.white)));
-        _help.setText("Help!");
+        _help.setText(R.string.button_label_help);
+        _help.setEnabled(true);
 
         _status_log.setTextColor(getColor((R.color.red)));
 
         _display_onoff.setText(R.string.display_on);
         _display_onoff.setTextSize(48);
         _display_onoff.setTextColor(getColor((R.color.red)));
-        _display_onoff.setBackground(getDrawable(R.drawable.borderline_red));
+        _display_onoff.setBackground(AppCompatResources.getDrawable(this, R.drawable.borderline_red));
 
         _Background.setBackgroundColor(getColor((R.color.white)));
     }
 
     /**
-     * dispaly mode change to fall_detected mode
+     * display mode change to fall_detected mode
      */
     private void fall_detected_ui_setup() {
         _status_log.setTextColor(getColor((R.color.white)));
 
-        _display_onoff.setText("Fall!");
+        _display_onoff.setText(R.string.label_Fall);
         _display_onoff.setTextSize(100);
         _display_onoff.setTextColor(getColor((R.color.white)));
-        _display_onoff.setBackground(getDrawable(R.drawable.borderline_white));
+        _display_onoff.setBackground(AppCompatResources.getDrawable(this, R.drawable.borderline_white));
 
         _onoff.setBackgroundColor(getColor((R.color.white)));
         _onoff.setTextColor(getColor((R.color.red)));
 
         _help.setBackgroundColor(getColor((R.color.white)));
         _help.setTextColor(getColor((R.color.red)));
-        _help.setText("I'm OK");
+        _help.setText(R.string.label_OK);
 
         _Background.setBackgroundColor(getColor((R.color.red)));
 
@@ -367,7 +384,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     /**
-     * dispaly mode change to emergency mode
+     * display mode change to emergency mode
      */
     private void emergency_ui_setup() {
         _Background.setBackgroundColor(getColor((R.color.rred)));
@@ -376,10 +393,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         _status_log.setText(R.string.EMERGENCY);
         _status_log.setTextColor(getColor((R.color.white)));
 
-        _display_onoff.setText("Fall!");
+        _display_onoff.setText(R.string.label_Fall);
         _display_onoff.setTextSize(100);
         _display_onoff.setTextColor(getColor((R.color.white)));
-        _display_onoff.setBackground(getDrawable(R.drawable.borderline_white));
+        _display_onoff.setBackground(AppCompatResources.getDrawable(this, R.drawable.borderline_white));
 
         _onoff.setTextColor(getColor((R.color.rred)));
         _onoff.setBackgroundColor(getColor((R.color.white)));
